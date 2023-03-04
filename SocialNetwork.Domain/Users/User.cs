c0fb8762fc62153ea67259323c2b76c1b57996c2
medegor44 <medegor44@gmail.com>
2023-03-04 15:48:19 +0300

    Add domain layer

---
+++ SocialNetwork.Domain/Users/User.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.eShopOnContainers.Services.Ordering.Domain.Seedwork;
+using SocialNetwork.Domain.Users.ValueObjects;
+
+namespace SocialNetwork.Domain.Users;
+
+public sealed class User : Entity<Guid>, IAggregateRoot
+{
+    public Guid CityId { get; }
+    public Name FirstName { get; }
+    public Name LastName { get; }
+    public Age Age { get; }
+    public Biography Biography { get; }
+    public IReadOnlyCollection<Guid> FriendIds { get; } = new List<Guid>();
+    public IReadOnlyCollection<Guid> PostsIds { get; } = new List<Guid>();
+
+    public User(Guid id, Name firstName, Name lastName, Age age, Biography biography, Guid cityId)
+    {
+        CityId = cityId;
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Biography = biography;
+        Id = id;
+    }
+
+    public User(
+        Guid id, 
+        Name firstName, 
+        Name lastName, 
+        Age age, 
+        Biography biography, 
+        Guid cityId,
+        IReadOnlyCollection<Guid> friendIds)
+        : this(id, firstName, lastName, age, biography, cityId)
+    {
+        FriendIds = friendIds;
+    }
+
+    public User(
+        Guid id, 
+        Name firstName, 
+        Name lastName, 
+        Age age, 
+        Biography biography, 
+        Guid cityId,
+        IReadOnlyCollection<Guid> friendIds, 
+        IReadOnlyCollection<Guid> postsIdsIds)
+        : this(id, firstName, lastName, age, biography, cityId, friendIds)
+    {
+        PostsIds = postsIdsIds;
+    }
+}
---
+++ SocialNetwork.Domain/Users/ValueObjects/Biography.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.eShopOnContainers.Services.Ordering.Domain.SeedWork;
+
+namespace SocialNetwork.Domain.Users.ValueObjects;
+
+public class Biography : ValueObject
+{
+    public Biography(string biography)
+    {
+        Value = biography ?? throw new ArgumentException("Must be not null", nameof(biography));
+    }
+
+    public string Value { get; }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
