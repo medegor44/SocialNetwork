fb031b1ed5dd16393226bf16c3d72a57074d1aee
egmedvedev <egmedvedev@ozon.ru>
2023-03-06 18:59:52 +0300

    Add migration project

---
+++ SocialNetwork.Migrations/Migrations/01_CreateUsers.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace SocialNetwork.Migrations.Migrations;
+
+[Migration(1)]
+public class CreateUsers : Migration 
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Id").AsGuid().PrimaryKey()
+            .WithColumn("FirstName").AsCustom("VARCHAR(100)")
+            .WithColumn("SecondName").AsCustom("VARCHAR(100)")
+            .WithColumn("Age").AsInt32()
+            .WithColumn("Biography").AsCustom("TEXT")
+            .WithColumn("CityId").AsGuid();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("Users");
+    }
+}
