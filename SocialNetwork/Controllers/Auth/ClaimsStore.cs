314b9651562baa42e4e37decb3031a23a69043d0
medegor44 <medegor44@gmail.com>
2023-04-29 10:57:30 +0300

    [HW-4] Add claims store, add friends controller

---
+++ SocialNetwork/Controllers/Auth/ClaimsStore.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+
+namespace SocialNetwork.Controllers.Auth;
+
+public class ClaimsStore : IClaimsStore
+{
+    private List<Claim> _claims = new();
+    private const string UserId = nameof(UserId);
+    
+    public ClaimsStore()
+    {
+    }
+
+    private ClaimsStore(IEnumerable<Claim> claims)
+    {
+        _claims = claims.ToList();
+    }
+
+    public IClaimsStore FromClaims(IEnumerable<Claim> claims) =>
+        new ClaimsStore(claims);
+
+    public IClaimsStore AddUserId(long userId) =>
+        new ClaimsStore(_claims.Concat(new[]
+        {
+            new Claim(UserId, userId.ToString())
+        }));
+
+    public long GetUserId() =>
+        long.Parse(_claims.First(x => x.Type == UserId).Value);
+
+    public IEnumerable<Claim> Claims =>
+        _claims;
+}
---
+++ SocialNetwork/Controllers/Friend/FriendController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Controllers.Auth;
+using SocialNetwork.Controllers.Common;
+
+namespace SocialNetwork.Controllers.Friend;
+
+[ApiController]
+public class FriendController : Controller
+{
+    private readonly ILogger<FriendController> _logger;
+
+    public FriendController(ILogger<FriendController> logger)
+    {
+        _logger = logger;
+    }
+    
+    [HttpPut("set/{friendId:long}")]
+    [ProducesResponseType(typeof(ActionFailedResponse), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ActionFailedResponse), StatusCodes.Status503ServiceUnavailable)]
+    [ProducesResponseType(typeof(ActionFailedResponse), StatusCodes.Status400BadRequest)]
+    [Authorize]
+    public Task<IResult> AddFriend([FromRoute] long friendId, [FromServices] IClaimsStore store)
+    {
+        var userId = store.FromClaims(HttpContext.User.Claims).GetUserId();
+        
+        _logger.LogInformation($"{userId} is friend to {friendId}");
+        
+        return Task.FromResult(Results.Ok());
+    }
+    
+    [HttpPut("delete/{friendId:long}")]
+    [ProducesResponseType(typeof(ActionFailedResponse), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ActionFailedResponse), StatusCodes.Status503ServiceUnavailable)]
+    [ProducesResponseType(typeof(ActionFailedResponse), StatusCodes.Status400BadRequest)]
+    [Authorize]
+    public Task<IResult> RemoveFriend([FromRoute] long friendId, [FromServices] IClaimsStore store)
+    {
+        var userId = store.FromClaims(HttpContext.User.Claims).GetUserId();
+        
+        _logger.LogInformation($"{userId} now is not friend to {friendId}");
+        
+        return Task.FromResult(Results.Ok());
+    }
+}
